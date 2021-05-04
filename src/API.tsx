e9f7e6b0eb4f66740a5d32d73425bbab73471489
seokwoo-park <p.seokwoo@gmail.com>
2021-05-04 23:00:03 +0200

    Questions and answers deployed

--- src/API.tsx
+++ src/API.tsx
@@ -12,13 +12,14 @@ export type Question = {
 export type QuestionState = Question & { answers: string[]}
 
 export enum Difficulty {
-    EASY = "easy",
-    MEDIUM = "medium",
-    HARD = "hard"
+    "easy",
+    "medium",
+    "hard"
 }
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=9&difficulty=${difficulty}&type=multiple`
+    console.log(endpoint)
     const data = await (await fetch(endpoint)).json()
     return data.results.map((question: Question) => (
         {
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,18 @@ import { fetchQuizQuestions } from './API'
 import { QuestionCard } from './components/index';
 
 // Types
-import { Difficulty } from './API'
+import { QuestionState ,Difficulty } from './API'
+
+// styles
+
+import {GlobalStyle, Wrapper} from './App.styles'
+
+export type AnswerObject = {
+  question: string
+  answer: string
+  correct: boolean
+  correctAnswer : string
+}
 
 
 const TOTAL_QUESTIONS = 10
@@ -13,46 +24,112 @@ const TOTAL_QUESTIONS = 10
 function App() {
 
   const [loading, setLoading] = useState(false)
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState<QuestionState[]>([])
   const [number, setNumber] = useState(0)
-  const [userAnswers, setUserAnswers] = useState([])
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(true)
-
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+  const [difficulty, setDifficulty] = useState<"easy"|"medium"|"hard">("easy")
 
   const startTrivia = async () => {
+    setLoading(true)
+    setGameOver(false)
 
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      difficulty
+    )
+
+    setQuestions(newQuestions)
+    setScore(0)
+    setUserAnswers([])
+    setNumber(0)
+
+    setLoading(false)
   }
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
 
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver){
+      const answer = e.currentTarget.value
+
+      const correct = questions[number].correct_answer === answer
+
+      if (correct) setScore(prev => prev + 1)
+
+      const answerObject:AnswerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      }
+      setUserAnswers((prev )=> [...prev, answerObject])
+    }
   }
 
   const nextQuestion = () => {
 
+    const nextQuestion = number + 1
+
+    if(nextQuestion === TOTAL_QUESTIONS){
+      setGameOver(true)
+    } else {
+      setNumber(nextQuestion)
+    }
   }
 
+  const onHandleChange =  (e: React.ChangeEvent<HTMLSelectElement>) :void =>  {
+    setDifficulty(e.currentTarget.value)
+    console.log(e.currentTarget.value)
+  }
+
+
   return (
-    <div className="App">
-      <h1>SIMPLE QUIZ</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">Score : </p>
-      <p>Loading Questions ...</p>
-      {/* <QuestionCard
-        questionNr={number +1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
-    </div>
+    <>
+      <GlobalStyle/>
+      <Wrapper>
+        <h1>SIMPLE QUIZ</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS 
+        ? (
+        <>
+          <label>
+          Choose Difficulty
+            <select value={difficulty} onChange={onHandleChange}>
+              <option value={Difficulty.EASY}>{Difficulty.EASY.toUpperCase()}</option>
+              <option value={Difficulty.MEDIUM}>{Difficulty.MEDIUM.toUpperCase()}</option>
+              <option value={Difficulty.HARD}>{Difficulty.HARD.toUpperCase()}</option>
+            </select>
+          </label>
+
+          <button className="start" onClick={startTrivia}>
+            Start
+          </button>
+        </>
+        ) 
+        : null}
+        { !gameOver ? <div className="score">Score : {score}</div> : null }
+        { loading && <p>Loading Questions ...</p>}
+        {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number +1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+        )}
+        {
+        !gameOver && 
+        !loading && 
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ?(
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+        ) : null }
+      </Wrapper>
+    </>
   );
 }
 
